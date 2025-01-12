7a9cfac269b21e56ced4b4ca7f7ac1a54b036e2d
Ryan <rrawlings01@gmail.com>
2025-01-12 01:26:59 -0600

    adding modifying user support failing

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin model for User model"""
+    ordering = ('email',)
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ['last_login', 'date_joined']
+
+admin.site.register(models.User, UserAdmin)
